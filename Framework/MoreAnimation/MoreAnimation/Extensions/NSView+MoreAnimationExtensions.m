21df9d600b4a4d03c44652a23dc4f82f898041d6
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-09-18 16:59:15 -0700

    Swizzle that shit

--- Framework/MoreAnimation/MoreAnimation/Extensions/NSView+MoreAnimationExtensions.m
+++ Framework/MoreAnimation/MoreAnimation/Extensions/NSView+MoreAnimationExtensions.m
@@ -8,34 +8,68 @@
 
 #import "NSView+MoreAnimationExtensions.h"
 #import "MALayer.h"
+#import "EXTRuntimeExtensions.h"
+#import "EXTSafeCategory.h"
 #import "EXTScope.h"
 
 // for associating an MALayer to an NSView
 static char * const NSViewAssociatedMALayerKey = "NSViewAssociatedMALayer";
 
-@implementation NSView (MoreAnimationExtensions)
+// variables for saving the old implementations
+static
+void (*originalDisplayRectInContextImpl)(NSView *, SEL, NSRect, NSGraphicsContext *) = NULL;
 
-#pragma mark NSView swizzles
+static
+void (*originalDrawLayerInContextImpl)(NSView *, SEL, MALayer *, CGContextRef) = NULL;
 
-- (void)displayRectIgnoringOpacity:(NSRect)rect inContext:(NSGraphicsContext *)context; {
+// NSView method overrides
+@interface NSViewMAMixin : NSView {}
+@end
+
+@implementation NSViewMAMixin
+
+- (void)displayRectIgnoringOpacity:(NSRect)rect inContext:(NSGraphicsContext *)context {
+	MALayer *layer = self.MALayer;
+	if (!layer) {
+		originalDisplayRectInContextImpl(self, _cmd, rect, context);
+		return;
+	}
+
 	[self.MALayer display];
 }
 
-#pragma mark MALayerDelegate
-
 - (void)drawLayer:(MALayer *)layer inContext:(CGContextRef)context {
+  	// 'layer' may be a CALayer, which means that NSView is meant to handle
+	// CALayerDelegate methods
+	if (![layer isKindOfClass:[MALayer class]]) {
+		originalDrawLayerInContextImpl(self, _cmd, layer, context);
+		return;
+	}
+
 	NSGraphicsContext *previousContext = [NSGraphicsContext currentContext];
 	@onExit {
 		[NSGraphicsContext setCurrentContext:previousContext];
 	};
 
-	NSGraphicsContext *graphicsContext = [NSGraphicsContext graphicsContextWithPort:context flipped:NO];
+	NSGraphicsContext *graphicsContext = [NSGraphicsContext graphicsContextWithGraphicsPort:context flipped:NO];
 	[NSGraphicsContext setCurrentContext:graphicsContext];
 	
   	[self lockFocus];
 	[self drawRect:self.bounds];
 	[self unlockFocus];
 }
+
+@end
+
+// perform injection after all runtime setup
+static
+__attribute__((constructor))
+void injectNSViewMAMixin (void) {
+	ext_replaceMethodsFromClass([NSViewMAMixin class], [NSView class]);
+}
+
+// simple additions
+@safecategory (NSView, MoreAnimationExtensions)
 
 #pragma mark Properties
 
