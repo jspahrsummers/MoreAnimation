1d990a9fd46ee9600695c3a649872d5334b66a2b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-09-20 02:26:19 -0700

    Added MAGeometry module, with support for multiplying a 3D vector by a CATransform3D

---
+++ Framework/MoreAnimation/MoreAnimation/Modules/MAGeometry.h
@@ -0,0 +1,25 @@
+//
+//  MAGeometry.h
+//  MoreAnimation
+//
+//  Created by Justin Spahr-Summers on 2011-09-20.
+//  Released into the public domain.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+typedef struct {
+	CGFloat x;
+	CGFloat y;
+	CGFloat z;
+	CGFloat w;
+} MAVector3D;
+
+#define MAVector3DMake(X, Y, Z, W) \
+	((MAVector3D){ .x = (X), .y = (Y), .z = (Z), .w = (W) })
+
+/**
+ * Transforms \a v using the given transformation matrix.
+ */
+MAVector3D MAVector3DApplyCATransform3D (MAVector3D v, CATransform3D t);
+
---
+++ Framework/MoreAnimation/MoreAnimation/Modules/MAGeometry.m
@@ -0,0 +1,19 @@
+//
+//  MAGeometry.c
+//  MoreAnimation
+//
+//  Created by Justin Spahr-Summers on 2011-09-20.
+//  Released into the public domain.
+//
+
+#import "MAGeometry.h"
+
+MAVector3D MAVector3DApplyCATransform3D (MAVector3D v, CATransform3D t) {
+    MAVector3D dst;
+    dst.x = v.x * t.m11 + v.y * t.m21 + v.z * t.m31 + v.w * t.m41;
+    dst.y = v.x * t.m12 + v.y * t.m22 + v.z * t.m32 + v.w * t.m42;
+    dst.z = v.x * t.m13 + v.y * t.m23 + v.z * t.m33 + v.w * t.m43;
+    dst.w = v.x * t.m14 + v.y * t.m24 + v.z * t.m34 + v.w * t.m44;
+    return dst;
+}
+
